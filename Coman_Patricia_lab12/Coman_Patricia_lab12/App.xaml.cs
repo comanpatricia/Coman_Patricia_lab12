63d55ffcf7aa0767fbcb6963bfc56c5d9e3b9b02
Patricia Coman <comanpatricia27@gmail.com>
2021-01-10 22:51:58 +0200

    Add project files.

---
+++ Coman_Patricia_lab12/Coman_Patricia_lab12/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Coman_Patricia_lab12.Data;
+
+
+namespace Coman_Patricia_lab12
+{
+    public partial class App : Application
+    {
+        public static ShoppingListDatabase Database { get; private set; }
+
+        public App()
+        {
+            Database = new ShoppingListDatabase(new RestService());
+            MainPage = new NavigationPage(new ListEntryPage());
+
+            //InitializeComponent();
+            //MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Coman_Patricia_lab12/Coman_Patricia_lab12/Data/IRestService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Coman_Patricia_lab12.Models;
+
+namespace Coman_Patricia_lab12.Data
+{
+   
+        public interface IRestService
+        {
+            Task<List<ShopList>> RefreshDataAsync();
+            Task SaveShopListAsync(ShopList item, bool isNewItem);
+            Task DeleteShopListAsync(int id);
+        }
+    
+}
